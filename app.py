d747cc2d551c06e16f60018b360eea983b5c4d40
sheikhareeba245 <silent5worker363@gmail.com>
2026-03-29 18:04:05 +0500

    Fix port for Railway

--- app.py
+++ app.py
@@ -494,4 +494,6 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
